a936ba895bfcc2362dd84bc7af80cfb3fadf651f
Che-Kang Wu <erickitten@gmail.com>
2014-12-17 23:04:53 +0800

    add max/min to pid output

--- src/pid.c
+++ src/pid.c
@@ -5,17 +5,23 @@ float runPID(pid_context_t * p,float setpoint,float input){
 
     float error;
     float deriv;
+    float output;
 
     error = setpoint - input;
     //use input instead of error to avoid derivative kick
     deriv = (input - p->prev_in) / p->dt;
 
-    p->integral += error * p->dt;
+    //intergal has already included ki part
+    p->integral += p->ki * error * p->dt;
 
-    if(p->integral > p->imax){p->integral = p->imax;}
-    if(p->integral > p->imin){p->integral = p->imin;}
+    if(p->integral > p->max){p->integral = p->max;}
+    if(p->integral < p->min){p->integral = p->min;}
 
     p->prev_in = input;
-    return error*p->kp + p->integral*p->ki + deriv*p->kd;
+    output =  error*p->kp + p->integral + deriv*p->kd;
+
+    if(output > p->max){output = p->max;}     
+    if(output < p->min){output = p->min;}
+    return output;
 }
 
