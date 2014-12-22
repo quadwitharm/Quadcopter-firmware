db7bec2b4b08621456b279947e28baa33f766347
chunikuo <chuni.kuo@gmail.com>
2014-12-23 01:35:50 +0800

    Add: the register address of HMC5883L & BMP180

--- inc/sensor/hmc5883l.h
+++ inc/sensor/hmc5883l.h
@@ -5,22 +5,44 @@ extern "C" {
 #endif
 
 #include "main.h"
-    struct HMC58831{
-        uint8_t XH;
-        uint8_t XL;
-        uint8_t YH;
-        uint8_t YL;
-        uint8_t ZH;
-        uint8_t ZL;
+    struct HMC5883L{
+	union{
+	    struct{
+		uint8_t XL;
+		uint8_t XH;
+		uint8_t YL;
+		uint8_t YH;
+		uint8_t ZL;
+		uint8_t ZH;
+	    }uint8;
+	    struct{
+		int16_t X;
+		int16_t Y;
+		int16_t Z;
+	    }int16;
+	};
     };
 
-    void HMC58831_Init();
-    void HMC58831_Recv(void*);
-    void HMC58831_Process(void*);
+    void HMC5883L_Init();
+    void HMC5883L_Recv(void*);
+    void HMC5883L_Process(void*);
 
     /* Device Address */
-#define _START
-
+//#define _START
+#define HMC5883L_START 	0x3C // Write 
+#define CRegA 		0x00
+#define CRegB 		0x01
+#define ModeReg 	0x02
+#define DataXMSB	0x03
+#define DataXLSB	0x04
+#define DataZMSB	0x05
+#define DataZLSB	0x06
+#define DataYMSB	0x07
+#define DataYLSB	0x08
+#define StatusReg	0x09
+#define IDRegA		0x10
+#define IDRegB		0x11
+#define IDRegC		0x12
 
 #ifdef __cplusplus
 }
