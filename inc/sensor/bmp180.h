5eb3208164274a0b36c62e6dd47e87b3d5c911f8
chunikuo <chuni.kuo@gmail.com>
2015-01-07 16:57:20 +0800

    Add: Calcuate temperature and pressure
    Can read temperature, and work fine
    the UP of pressure cannot be read, need to fix

--- inc/sensor/bmp180.h
+++ inc/sensor/bmp180.h
@@ -6,12 +6,45 @@ extern "C" {
 
 #include "main.h"
     struct BMP180{
-        uint8_t XH;
-        uint8_t XL;
-        uint8_t YH;
-        uint8_t YL;
-        uint8_t ZH;
-        uint8_t ZL;
+	union {
+	    struct {
+                uint8_t AC1H;
+                uint8_t AC1L;
+                uint8_t AC2H;
+                uint8_t AC2L;
+                uint8_t AC3H;
+                uint8_t AC3L;
+                uint8_t AC4H;
+                uint8_t AC4L;
+                uint8_t AC5H;
+                uint8_t AC5L;
+                uint8_t AC6H;
+                uint8_t AC6L;
+                uint8_t B1H;
+                uint8_t B1L;
+                uint8_t B2H;
+                uint8_t B2L;
+                uint8_t MBH;
+                uint8_t MBL;
+                uint8_t MCH;
+                uint8_t MCL;
+                uint8_t MDH;
+                uint8_t MDL;
+	    }uint8;
+	    struct {
+		int16_t AC1;
+		int16_t AC2;
+		int16_t AC3;
+		uint16_t AC4;
+		uint16_t AC5;
+		uint16_t AC6;
+		int16_t B1;
+		int16_t B2;
+		int16_t MB;
+		int16_t MC;
+		int16_t MD;
+	    }int16;
+	};
     };
 
     void BMP180_Init();
@@ -20,7 +53,32 @@ extern "C" {
 
     /* Device Address */
 #define BMP180_START 0xEE
-
+#define AC1HREG      0xAA
+#define AC1LREG      0xAB
+#define AC2HREG	     0xAC
+#define AC2LREG      0xAD
+#define AC3HREG      0xAE
+#define AC3LREG      0xAF
+#define AC4HREG	     0xB0
+#define AC4LREG      0xB1
+#define AC5HREG      0xB2
+#define AC5LREG      0xB3
+#define AC6HREG	     0xB4
+#define AC6LREG      0xB5
+#define B1HREG       0xB6
+#define B1LREG       0xB7
+#define B2HREG	     0xB8
+#define B2LREG       0xB9
+#define MBHREG       0xBA
+#define MBLREG       0xBB
+#define MCHREG	     0xBC
+#define MCLREG       0xBD
+#define MDHREG	     0xBE
+#define MDLREG       0xBF
+#define TEMPERATURE  0x2E
+#define PRESSURE0    0x34
+#define CRLREG	     0xF4
+#define OverSampling 3
 
 #ifdef __cplusplus
 }
---
+++ src/sensor/bmp180.c
@@ -0,0 +1,153 @@
+#include "sensor/bmp180.h"
+#include "sensor/sensor.h"
+#include "sensor/i2c.h"
+
+#include "task.h"
+#include "semphr.h"
+
+static xSemaphoreHandle BMP180_Lock;
+static bool dataAvailable = false;
+struct BMP180 BMP180;
+long UT;
+long UP;
+
+void Write_BMP180(uint8_t Register, uint8_t content){
+    uint8_t buf[2];
+    buf[0] = Register;
+    buf[1] = content;
+    while(!I2C_Master_Transmit(BMP180_START, buf, 2));
+}
+
+void READ_BMP180(uint8_t addr, uint8_t buf[], uint8_t size){
+    buf[0] = addr;
+    do{
+        I2C_Master_Transmit(BMP180_START, buf, size);
+    }while(!I2C_Master_Receive(BMP180_START, buf, size));
+};
+
+void BMP180_Init(){
+    BMP180_Lock = xSemaphoreCreateMutex();
+
+    kputs("Setting Control Register for BMP180\r\n");
+
+    uint8_t data[2] = {0};
+    READ_BMP180(AC1HREG, data, 2);
+    BMP180.uint8.AC1H = data[0];
+    BMP180.uint8.AC1L = data[1];
+
+    READ_BMP180(AC2HREG, data, 2);
+    BMP180.uint8.AC2H = data[0];
+    BMP180.uint8.AC2L = data[1];
+
+    READ_BMP180(AC3HREG, data, 2);
+    BMP180.uint8.AC3H = data[0];
+    BMP180.uint8.AC3L = data[1];
+
+    READ_BMP180(AC4HREG, data, 2);
+    BMP180.uint8.AC4H = data[0];
+    BMP180.uint8.AC4L = data[1];
+
+    READ_BMP180(AC5HREG, data, 2);
+    BMP180.uint8.AC5H = data[0];
+    BMP180.uint8.AC5L = data[1];
+
+    READ_BMP180(AC6HREG, data, 2);
+    BMP180.uint8.AC6H = data[0];
+    BMP180.uint8.AC6L = data[1];
+
+    READ_BMP180(B1HREG, data, 2);
+    BMP180.uint8.B1H = data[0];
+    BMP180.uint8.B1L = data[1];
+
+    READ_BMP180(B2HREG, data, 2);
+    BMP180.uint8.B2H = data[0];
+    BMP180.uint8.B2L = data[1];
+
+    READ_BMP180(MBHREG, data, 2);
+    BMP180.uint8.MBH = data[0];
+    BMP180.uint8.MBL = data[1];
+
+    READ_BMP180(MCHREG, data, 2);
+    BMP180.uint8.MCH = data[0];
+    BMP180.uint8.MCL = data[1];
+
+    READ_BMP180(MDHREG, data, 2);
+    BMP180.uint8.MDH = data[0];
+    BMP180.uint8.MDL = data[1];
+
+    kputs("Control Register for BMP180 had been set\r\n");
+}
+
+void BMP180_Recv(void *arg){
+    while(1){
+        xSemaphoreTake( BMP180_Lock, ( portTickType ) portMAX_DELAY );
+
+	// Read UT	
+	Write_BMP180(CRLREG, TEMPERATURE);
+	vTaskDelay(5);
+	uint8_t dataT[2] = {0};
+	READ_BMP180(0xF6, dataT, 2);
+
+	UT = dataT[0] << 8 | dataT[1];
+
+	// Read UP, some error, cannot read continueously
+	// this will make the I2C susspend...Orz
+	/*
+	Write_BMP180(CRLREG, PRESSURE0 | (OverSampling << 6));
+	vTaskDelay(26);
+	uint8_t dataP[3] = {0};
+	READ_BMP180(0xF6, dataP, 3);
+
+	UP = dataP[0] << 16 | dataP[1] << 8 | dataP[2] >> (8-OverSampling);
+	*/
+        dataAvailable = true;
+        xSemaphoreGive( BMP180_Lock );
+    }
+}
+void BMP180_Process(void *arg){
+    xSemaphoreTake( BMP180_Lock, ( portTickType ) portMAX_DELAY );
+
+    /* Assume data will be proccessed before next read to data */
+    if(!dataAvailable){
+        xSemaphoreGive( BMP180_Lock );
+        return;
+    }
+    dataAvailable = false;
+
+    /* Calcualte Temperature */
+    long x1 = (((long)UT - (long)BMP180.int16.AC6) * (long)BMP180.int16.AC5) >> 15;
+    long x2 = ((long)BMP180.int16.MC << 11) / (x1 + BMP180.int16.MD);
+    long B5 = x1 + x2;
+    // temp = (B5 + 8) >>4;
+    // Temperature = temp;
+ 
+    /* Calculate Pressure */
+    // Calculate B3
+    long B6 = B5 - 4000;
+    x1 = (BMP180.int16.B2 * ((B6 * B6) >> 12)) >> 11;
+    x2 = (BMP180.int16.AC2 * B6) >> 11;
+    long x3 = x1 + x2;
+    long B3 = ((((((long)BMP180.int16.AC1)<<2) + x3)<<OverSampling) + 2)>>2;
+
+    // Calculate B4
+    x1 = (BMP180.int16.AC3 * B6) >> 13;
+    x2 = (BMP180.int16.B1 * ((B6 * B6) >> 12)) >> 16;
+    x3 = ((x1 + x2) + 2)>>2;
+    long B4 = (BMP180.int16.AC4 * (unsigned long)(x3 + 32768)) >> 15;
+
+    long B7 = ((unsigned long)(UP-B3) * (50000 >> OverSampling));
+    long press = 0;
+    if (B7 < 0x80000000)
+        press = (B7<<1)/B4;
+    else
+        press = (B7/B4)<<1;
+
+    x1 = (press>>8)*(press>>8);
+    x1 = (x1*3038)>>16;
+    x2 = (-7357 * press)>>16;
+    press += (x1 + x2 + 3791)>>4;
+    // Pressure = press in Pa;
+    //vAttitude.pitch = p;
+
+    xSemaphoreGive( BMP180_Lock );
+}
