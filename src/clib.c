8671825f2d4c99ca47978b2d5011307e70cec35d
Che-Kang Wu <erickitten@gmail.com>
2015-01-14 16:01:20 +0800

    modify clib output functions to use new protocol

--- src/clib.c
+++ src/clib.c
@@ -26,13 +26,27 @@ int vsnprintf (char * s, size_t n, const char * format, va_list arg );
  * @brief  k* I/O is for debugging, not interrupt based.
  */
 void kputs(const char *str){
+    uint8_t b = 0x03;
+    uint16_t len=0;
+
+    UART_send(&b,1);
+    while(str[len]){
+        len++;
+    }
+    UART_send((uint8_t *)&len,2);
     while(*str){
         UART_send((uint8_t *)str++,1);
     }
 }
 void kputc(const char c){
+    uint8_t b = 0x03;
+    uint16_t len=1;
+
+    UART_send(&b,1);
+    UART_send((uint8_t *)&len,2);
     UART_send((uint8_t *)&c,1);
 }
+
 void kgets(char buf[],int len){
     int i = 0;
     while(i < len - 1){
@@ -65,13 +79,30 @@ int kprintf(const char *format, ...){
  * @brief  Interrupt based I/O
  */
 void puts(const char *str){
+    uint16_t len=0;
+
+    send_byte((uint8_t)0x03);
+    while(str[len]){
+        len++;
+    }
+    send_byte((uint8_t)len>>8);
+    send_byte((uint8_t)len);
     while(*str){
         send_byte(*str++);
     }
+
 }
 void putc(const char c){
+    uint8_t b = 0x03;
+    uint16_t len=1;
+
+    send_byte(b);
+    send_byte((uint8_t)len>>8);
+    send_byte((uint8_t)len);
+
     send_byte(c);
 }
+
 void gets(char buf[],int len){
     int i = 0;
     while(i < len - 1){
