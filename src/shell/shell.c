8f2a73cf05b353b110e7daaa315c4bdea7f62ba1
Che-Kang Wu <erickitten@gmail.com>
2015-01-14 19:51:59 +0800

    add protocol input handlers to shell
    handleTextCommand ,handleChangeSetPoint ,handlePID

--- src/shell/shell.c
+++ src/shell/shell.c
@@ -3,6 +3,7 @@
 
 #include "sensor/sensor.h"
 #include "uart.h"
+#include "controller/control_api.h"
 
 #define BUFSIZE 128
 #define ARGV_SIZE 20
@@ -10,6 +11,9 @@
 static xTaskHandle shellTaskHandle;
 
 static void ShellTask(void *);
+static void handlePID();
+static void handleChangeSetPoint();
+static void handleTextCommand();
 static int parseCommand(char *line, char **argv);
 static commandfunc_t findCommand(char *name);
 
@@ -36,21 +40,75 @@ bool InitShell(){
 }
 
 static void ShellTask(void *args){
+    uint8_t type;
+    // Interrupt based I/O
+    //puts("Welcome to quadcopter shell!\r\n");
+    while(1){
+        //puts( COMMAND_PROMPT );
+        //gets(line, BUFSIZE);
+        UART_recv_IT(&type,1);//get command type
+
+        switch(type){
+            case 0x0:
+            case 0x1:
+            case 0x2:
+            case 0x3:break;
+            case 0x4:
+                handlePID();
+                break;
+            case 0x5:
+                handleChangeSetPoint();
+                break;
+            case 0x6:
+                handleTextCommand();
+                break;
+        }
+    }
+}
+
+static void handlePID(){
+    uint8_t which;
+    float buf[3];
+
+    UART_recv_IT(&which,1);
+    UART_recv_IT((uint8_t *)buf,12);
+    setPidParameter(which,KP,buf[0]);
+    setPidParameter(which,KI,buf[1]);
+    setPidParameter(which,KD,buf[2]);
+}
+
+static void handleChangeSetPoint(){
+    uint8_t op;
+    float buf[3];
+
+    UART_recv_IT(&op,1);
+    if(op == 0x0){
+        UART_recv_IT((uint8_t *)buf,12);
+        setSetPoint(ROLL_C,buf[0]);
+        setSetPoint(PITCH_C,buf[1]);
+        setSetPoint(YAW_C,buf[2]);
+    }else if(op == 0x1){
+        UART_recv_IT((uint8_t *)buf,4);
+        setSetPoint(THR_C,buf[0]);
+    }else{
+        
+    }
+}   
+
+static void handleTextCommand(){
+    uint16_t len;
     char line[BUFSIZE];
     char *argv[ARGV_SIZE];
-    // Interrupt based I/O
-    puts("Welcome to quadcopter shell!\r\n");
-    while(1){
-        puts( COMMAND_PROMPT );
-        gets(line, BUFSIZE);
+    
+    UART_recv_IT((uint8_t*)&len,2);
+    UART_recv_IT((uint8_t*)line,len);
 
-        int argc = parseCommand(line, argv);
-        commandfunc_t toExec = findCommand(argv[0]);
-        if(toExec == NULL){
-            puts("Command not found!\r\n");
-        }else{
-            toExec(argc,argv);
-        }
+    int argc = parseCommand(line, argv);
+    commandfunc_t toExec = findCommand(argv[0]);
+    if(toExec == NULL){
+        puts("Command not found!\r\n");
+    }else{
+        toExec(argc,argv);
     }
 }
 
