7e8914497ce958cd467b11a9f25857bd3f4a9877
Che-Kang Wu <erickitten@gmail.com>
2015-08-18 01:46:58 +0800

    fix multiple init & indent in mspinit
    gpios were init one by one, now they all init in one gpio init call.
    make mspinit more readable & fix indent.
    also, use array to store spi handlers.

--- src/spi.c
+++ src/spi.c
@@ -27,7 +27,8 @@
 #define SPIx_IRQn                        SPI4_IRQn
 #define SPIx_IRQHandler                  SPI4_IRQHandler
 
-SPI_HandleTypeDef Spi1Handle , Spi2Handle;
+//[0] -> spi1 ,[1] -> spi2
+SPI_HandleTypeDef SpiHandle[2];
 
 
 bool SPI_init(void){
@@ -44,7 +45,7 @@ bool SPI_init(void){
      *  Configure the interrupts but do not enable.
      * */
 
-	Spi1Handle = (SPI_HandleTypeDef) {
+	SpiHandle[0] = (SPI_HandleTypeDef) {
 		.Instance = SPI1,
 		.Init = {
 			.Mode = SPI_MODE_MASTER,
@@ -61,7 +62,7 @@ bool SPI_init(void){
 		}
 	};
 
-	Spi2Handle = (SPI_HandleTypeDef) {
+	SpiHandle[1] = (SPI_HandleTypeDef) {
 		.Instance = SPI2,
 		.Init = {
 			.Mode = SPI_MODE_MASTER,
@@ -79,8 +80,8 @@ bool SPI_init(void){
 	};
 
 
-	return (HAL_SPI_Init(&Spi1Handle) == HAL_OK) &&
-		(HAL_SPI_Init(&Spi2Handle) == HAL_OK);
+	return (HAL_SPI_Init(&SpiHandle[0]) == HAL_OK) &&
+		(HAL_SPI_Init(&SpiHandle[1]) == HAL_OK);
 }
 
 void SPI_send(int nspi,uint8_t* data, uint16_t length){
@@ -132,67 +133,55 @@ void StartSPIRXInterrupt(){
 }
 
 
-void HAL_SPI_MspInit(SPI_HandleTypeDef *hspi){	
+void HAL_SPI_MspInit(SPI_HandleTypeDef *hspi){
+
 	GPIO_InitTypeDef GPIO_InitStruct = { 
 		.Mode=GPIO_MODE_AF_PP, 
 		.Pull=GPIO_PULLUP,
 		.Speed=GPIO_SPEED_FAST
 	};
 
-    /* SPI1:
-     *  + SCK:  PB3
-     *  + MISO: PB4
-     *  + MOSI: PB5
-     *
-     * SPI2:
-     *  + SCK:  PB13
-     *  + MISO: PB14
-     *  + MOSI: PB15
-    */ 
-  
-    if(hspi->Instance ==SPI1){
-        /*##-1- Enable peripherals and GPIO Clocks #################################*/
-        /* Enable SPI clock */
-        __SPI1_CLK_ENABLE();
-        /* Enable GPIO TX/RX clock */
-        __GPIOB_CLK_ENABLE();
+	/* SPI1:
+	 *  + SCK:  PB3
+	 *  + MISO: PB4
+	 *  + MOSI: PB5
+	 *
+	 * SPI2:
+	 *  + SCK:  PB13
+	 *  + MISO: PB14
+	 *  + MOSI: PB15
+	 */ 
 
-        /*##-2- Configure peripheral GPIO ##########################################*/  
-        /* SPI SCK GPIO pin configuration  */
-        GPIO_InitStruct.Pin = GPIO_PIN_3;
-        GPIO_InitStruct.Alternate = GPIO_AF5_SPI1;
-        HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+	if(hspi->Instance ==SPI1){
+		/*##-1- Enable peripherals and GPIO Clocks ###########################*/
+		/* Enable SPI clock */
+		__SPI1_CLK_ENABLE();
+		/* Enable GPIO TX/RX clock */
+		__GPIOB_CLK_ENABLE();
 
-        /* SPI MISO GPIO pin configuration  */
-        GPIO_InitStruct.Pin = GPIO_PIN_4;
-        HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+		/*##-2- Configure peripheral GPIO ####################################*/
+		/* SPI SCK,MISO,MOSI GPIO pin configuration  */
+		GPIO_InitStruct.Pin = GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5;
+		GPIO_InitStruct.Alternate = GPIO_AF5_SPI1;
+		HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-        /* SPI MOSI GPIO pin configuration  */
-        GPIO_InitStruct.Pin = GPIO_PIN_5;
-        HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+		/*##-3- Configure the NVIC for SPI ###################################*/
+		/* NVIC for SPI */
+		HAL_NVIC_SetPriority(SPI1_IRQn, 12, 0);
+		HAL_NVIC_EnableIRQ(SPI1_IRQn);
 
-        /*##-3- Configure the NVIC for SPI #########################################*/
-        /* NVIC for SPI */
-        HAL_NVIC_SetPriority(SPI1_IRQn, 12, 0);
-        HAL_NVIC_EnableIRQ(SPI1_IRQn);
-    }else if(hspi->Instance ==SPI2){
-        __SPI2_CLK_ENABLE();
-        __GPIOB_CLK_ENABLE();
+	}else if(hspi->Instance ==SPI2){
+		__SPI2_CLK_ENABLE();
+		__GPIOB_CLK_ENABLE();
 
-        GPIO_InitStruct.Pin = GPIO_PIN_13;
-        GPIO_InitStruct.Alternate = GPIO_AF5_SPI2;
-        HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-        GPIO_InitStruct.Pin = GPIO_PIN_14;
-        HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
-
-        GPIO_InitStruct.Pin = GPIO_PIN_15;
-        HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+		GPIO_InitStruct.Pin = GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15;
+		GPIO_InitStruct.Alternate = GPIO_AF5_SPI2;
+		HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 		HAL_NVIC_SetPriority(SPI2_IRQn, 12, 0);
 		HAL_NVIC_EnableIRQ(SPI2_IRQn);
 
-    }
+	}
 }
 
 
