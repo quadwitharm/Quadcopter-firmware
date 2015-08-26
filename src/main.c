5c283dbeb0a3eaae0a5c3331d3e38042139c2422
ItsEddy <itseddy0402@gmail.com>
2015-08-26 21:28:34 +0800

    NRF24L01 test code

--- src/main.c
+++ src/main.c
@@ -13,6 +13,16 @@
 #include "controller/control_task.h"
 #include "shell/shell.h"
 #include "shell/send.h"
+#include "shell/nrf24l01.h"
+
+void NRFTestTask(void *args){
+    uint8_t buf[32];
+    NRF24L01_Transmit(1, (uint8_t *)"It works!\r\n", 11);
+    UART_send((uint8_t *)"Sent. try to receive\r\n", 22);
+    NRF24L01_Receive(0, buf, 11);
+    UART_send(buf, 11);
+    while(1);
+}
 
 int main(void){
     HAL_Init();
@@ -23,26 +33,38 @@ int main(void){
         while(1);
     }
     Init_IO();
-    UART_send((uint8_t []){(uint8_t)0xFF}, 1);
-    kputs("USART test\r\n");
+//    UART_send((uint8_t []){(uint8_t)0xFF}, 1);
+//    kputs("USART test\r\n");
+//    if(!InitSensorPeriph() || !InitSensorTask()){
+//        kputs("Initialize sensor task failed!\r\n");
+//    }
+//    if(!Init_Motor()){
+//        kputs("Initialize motor failed!\r\n");
+//    }
+//    if(!Init_Controller()){
+//        kputs("Initialize controller task failed!\r\n");
+//    }
+//    if(!InitShell()){
+//        kputs("Initialize shell task failed!\r\n");
+//    }
+//    if(!Init_SendInfoTask()){
+//        kputs("Initialize information task failed!\r\n");
+//    }
 
-    if(!InitSensorPeriph() || !InitSensorTask()){
-        kputs("Initialize sensor task failed!\r\n");
-    }
-    if(!Init_Motor()){
-        kputs("Initialize motor failed!\r\n");
-    }
-    if(!Init_Controller()){
-        kputs("Initialize controller task failed!\r\n");
-    }
-    if(!InitShell()){
-        kputs("Initialize shell task failed!\r\n");
-    }
-    if(!Init_SendInfoTask()){
-        kputs("Initialize information task failed!\r\n");
-    }
+//    kputs("Initialization complete, Start schedular!\r\n");
 
-    kputs("Initialization complete, Start schedular!\r\n");
+    UART_send((uint8_t *)"Test \r\n", 7);
+    if(!NRF24L01_Init()){
+        UART_send((uint8_t *)"Init failed \r\n", 14);
+    }
+    xTaskCreate(NRFTestTask,
+                "NRF IRQ Handling Task",
+                256,
+                NULL,
+                tskIDLE_PRIORITY + 6,
+                NULL
+                );
+
 //    schestart = 1;
     vTaskStartScheduler();
 
